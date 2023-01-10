eba7df5b92a68161ce7fe5e595291b5d1bc8e4c8
Murad Khudiev <mkhudiev99@gmail.com>
2023-01-10 15:11:32 -0800

    premerge

--- blog_service/routers/blogs.py
+++ blog_service/routers/blogs.py
@@ -5,9 +5,9 @@ from typing import Union, List
 router = APIRouter()
 
 @router.get("/blogs", response_model=Union[Error, List[BlogList]])
-def get_all(
+def all_blogs(
     blogs: BlogList,
     repo: BlogRepository = Depends(),
 ):
-    return repo.get_all(blogs)
+    return repo.all_blogs(blogs)
     
