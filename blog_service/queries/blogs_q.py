dbe6f01252fc30bc4e6c09ba5bb663c5bc96fbb9
Allen <achenins@gmail.com>
2023-01-10 15:50:48 -0700

    added pool.py, added BlogRepo, compeleted create and delete blogs

--- blog_service/queries/blogs_q.py
+++ blog_service/queries/blogs_q.py
@@ -1,8 +1,46 @@
 from pydantic import BaseModel
 from datetime import date
+from queries.pool import pool
+
+class BlogError(BaseModel):
+    message: str
 
 class BlogIn(BaseModel):
     username: str
     post_date: date
     pic_url: str
     description: str
+
+class BlogOut(BaseModel):
+    id: int
+    username: str
+    post_date: date
+    pic_url: str
+    description: str
+
+
+class BlogRepo:
+    def create(self, blog: BlogIn) -> BlogOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO blogs
+                            (username, post_date, pic_url, description)
+                        VALUES
+                            (%s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            blog.username,
+                            blog.post_date,
+                            blog.pic_url,
+                            blog.description
+                        ]
+                    )
+                    id = result.fetchone()[0]
+                    old_data = blog.dict()
+                    return BlogOut(id=id, **old_data)
+        except Exception:
+            return {"message": "Could not create new blog!"}
--- blog_service/routers/blogs_r.py
+++ blog_service/routers/blogs_r.py
@@ -1,9 +1,18 @@
-from fastapi import APIRouter
-from queries.blogs_q import BlogIn
+from fastapi import APIRouter, Depends, Response
+from typing import Union
+from queries.blogs_q import (
+    BlogError,
+    BlogIn,
+    BlogRepo,
+    BlogOut,
+)
 
 router = APIRouter()
 
-@router.post("/blogs")
-def create_blog(blog: BlogIn):
-    print(blog)
-    return blog
+@router.post("/blogs", response_model=Union[BlogOut, BlogError])
+def create_blog(blog: BlogIn, response: Response, repo: BlogRepo = Depends()):
+    return repo.create(blog)
+
+@router.delete("/blogs/{blog_id}", response_model=bool)
+def delete_blog(blog_id: int, repo: BlogRepo = Depends()) -> bool:
+    return repo.delete(blog_id)
