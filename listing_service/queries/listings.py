29d25adfbdff96809d7dc805c609bea69338c76a
Brandon Matran <b_matran@comcast.net>
2023-01-17 12:41:05 -0800

    Merging

--- listing_service/queries/listings.py
+++ listing_service/queries/listings.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Union, List, Optional
 from datetime import date
 from queries.pool import pool
 
@@ -27,6 +28,34 @@ class ListingOut(BaseModel):
 
 
 class ListingRepository:
+    def all_listings(self) -> Union[ListingError, List[ListingOut]]:
+        # try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id, title, company_name, job_position, apply_url, deadline, created
+                        FROM listings
+                        ORDER BY created;
+                        """
+                    )
+
+                    return [
+                            ListingOut(
+                                id=record[0],
+                                title=record[1],
+                                company_name=record[2],
+                                job_position=record[3],
+                                apply_url=record[4],
+                                deadline=record[5],
+                                created=record[6],
+                            )
+                            for record in db
+                    ]
+        # except Exception as e:
+        #     print(e)
+        #     return {"message": "Failed to display Job Listings"}
+
     def create(self, listing: ListingIn) -> ListingOut:
         try:
             with pool.connection() as conn:
