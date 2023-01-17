f5a7fd0edb5afcacc4d454cec178d5ac7fa3c23a
Brandon Matran <b_matran@comcast.net>
2023-01-17 12:23:46 -0800

    Main page FrontEnd

--- ghi/src/accounts/MainPage.js
+++ ghi/src/accounts/MainPage.js
@@ -1,9 +1,10 @@
 import background_image from "../images/background_image.png";
 import programmer from "../images/programmer.jpg";
 import { useNavigate } from "react-router";
-import './MainPage.css';
-import { getToken, useToken } from "./Authentication";
+import "./MainPage.css";
 import { useEffect, useState } from "react";
+import LoginModal from "./LoginModal";
+import SignUpModal from "./SignupModal";
 
 function Column(props) {
   return (
@@ -19,9 +20,7 @@ function Column(props) {
               <img src={data.pic_url} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{data.venue}</h5>
-                <p className="card-text">
-                  Date: {date}
-                </p>
+                <p className="card-text">Date: {date}</p>
 
                 <p className="card-text">Description: {data.description}</p>
               </div>
@@ -33,37 +32,35 @@ function Column(props) {
   );
 }
 
-
 function MainPage() {
-  const [token] = useToken();
   const navigate = useNavigate();
-  const [blogs, setBlogList] = useState([], [], [])
+  const [loginModal, setLoginModal] = useState(false);
+  const [signupModal, setSignUpModal] = useState(false);
+  const [blogs, setBlogList] = useState([], [], []);
 
-
-useEffect(() => {
+  useEffect(() => {
     const url = "http://localhost:8080/blogs";
     async function fetchData() {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
+        const blogData = data.slice(data.length - 3);
         const blogs = [[], [], []];
         let i = 0;
-        for (const event of data) {
+
+        for (const event of blogData) {
           blogs[i].push(event);
           i = i + 1;
           if (i > 2) {
             i = 0;
           }
+
+          setBlogList(blogs);
         }
-        console.log(blogs.length)
-        setBlogList(blogs);
-
-
+      }
     }
-  }
     fetchData();
   }, []);
-
 
   const signupClick = async (e) => {
     navigate("/signup");
@@ -71,64 +68,74 @@ useEffect(() => {
 
   const LoginClick = async (e) => {
     navigate("/login");
-  }
-
-
+  };
 
   return (
-    <section
-      className="vh-100"
-      style={{ backgroundImage: `url(${background_image})` }}
-    >
-      <div className="container h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col-lg-12 col-xl-11">
-            <div className="card text-black">
-              <div className="card-body p-md-5">
-                <div className="row justify-content-center">
-                  <div className="w-100">
-                    <div>
-                      <div className="container max-width">
-                        <div className="d-flex flex-row-reverse">
-                          <button
-                            onClick={() => signupClick()}
-                            type="button"
-                            className="signup btn btn-primary btn-lg text-right"
-                          >
-                            Sign Up
-                          </button>
-                        </div>
-                        <div className="d-flex flex-row-reverse">
-                          <button
-                            onClick={() => LoginClick()}
-                            type="button"
-                            className="signup btn btn-primary btn-lg text-right"
-                          >
-                            Log In
-                          </button>
-                        </div>
-                      </div>
-                      <img
-                        src={programmer}
-                        className="programmer img-fluid"
-                        alt="programmer"
-                      />
-                    </div>
+    <div>
+      <div
+        className="header"
+        style={{ backgroundImage: `url(${background_image})` }}
+      >
+        <div id="logo">Anchor ⚓</div>
+      </div>
+      <div className="middle">
+          <div>
+            <div className="container-fluid">
+              <img
+                src={programmer}
+                className="programmer img-fluid"
+                alt="programmer"
+              />
+            </div>
+            <div className="container">
+              <div className="blogContainer max-width">
+                <div className="d-flex flex-row-reverse">
+                  <button
+                    onClick={() => {
+                      setSignUpModal(true);
+                    }}
+                    type="button"
+                    className="openSignupModal"
+                  >
+                    Sign Up
+                  </button>
+                  {signupModal && (
+                    <SignUpModal closeSignupModal={setSignUpModal} />
+                  )}
+                </div>
+                <button
+                  className="openLoginModal"
+                  onClick={() => setLoginModal(true)}
+                >
+                  Log In
+                </button>
+                {loginModal && <LoginModal closeLoginModal={setLoginModal} />}
+              </div>
+            </div>
+            <div className="justify">
+              <div className="aboveFooter">
+                <div>
+                  <div className="row blogRow">
+                    {blogs.map((blog, index) => {
+                      return <Column key={index} list={blog} />;
+                    })}
                   </div>
                 </div>
               </div>
             </div>
-            <div className="container">
-              <div className="row">
-                {blogs.map((blog, index) => {
-                  return <Column key={index} list={blog} />;
-                })}
-              </div>
-            </div>
-          </div>
+
         </div>
       </div>
-    </section>
+
+      <div className="container" id="footer-container">
+        <footer
+          className="footer"
+          id="footer"
+          style={{ backgroundImage: `url(${background_image})` }}
+        >
+        </footer>
+      </div>
+    </div>
   );
 }
 
