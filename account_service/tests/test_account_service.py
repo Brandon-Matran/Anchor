09549d2a8af624169741d0fa170b26b0082a23b8
Brandon Matran <b_matran@comcast.net>
2023-01-20 15:12:22 -0800

    Testing Unit Test

--- account_service/tests/test_account_service.py
+++ account_service/tests/test_account_service.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 from main import app
 from queries.accounts import AccountQueries
-# from routers.accounts import get_authenticator
+from routers.accounts import get_authenticator
 from jwtdown_fastapi.authentication import Token
 
 
@@ -20,19 +20,19 @@ expected_get_response = {
     "user_type": "individual",
 }
 
-# expected_create_response = {
-#     "access_token": "asfivuabwoiurbaw3iruabwvknsd",
-#     "token_type": "Bearer",
-#     "account": {"id": "1", "username": "test", "user_type": "individual"},
-# }
+expected_create_response = {
+    "access_token": "asfivuabwoiurbaw3iruabwvknsd",
+    "token_type": "Bearer",
+    "account": {"id": "1", "username": "test", "user_type": "individual"},
+}
 
 
 class MockAccountQueries:
     def get(self, username):
         pass
 
-    # def create(self, new_account, new_pass):
-    #     return expected_post_response
+    def create(self, new_account, new_pass):
+        return expected_post_response
 
     def get_all(self):
         return [expected_post_response]
@@ -74,25 +74,25 @@ def test_get_accounts():
     app.dependency_overrides = {}
 
 
-# def test_create_accounts():
+def test_create_accounts():
 
-#     req_body = {
-#         "username": "test",
-#         "password": "test",
-#         "user_type": "individual",
-#     }
+    req_body = {
+        "username": "test",
+        "password": "test",
+        "user_type": "individual",
+    }
 
-#     # Arrange
-#     app.dependency_overrides[AccountQueries] = MockAccountQueries
-#     app.dependency_overrides[get_authenticator] = mock_get_authenticator
+    # Arrange
+    app.dependency_overrides[AccountQueries] = MockAccountQueries
+    app.dependency_overrides[get_authenticator] = mock_get_authenticator
 
-#     # Act
-#     response = client.post("/api/accounts", json=req_body)
-#     actual = response.json()
+    # Act
+    response = client.post("/api/accounts", json=req_body)
+    actual = response.json()
 
-#     # Assert
-#     assert response.status_code == 200
-#     assert actual == expected_create_response
+    # Assert
+    assert response.status_code == 200
+    assert actual == expected_create_response
 
-#     # cleanup
-#     app.dependency_overrides = {}
+    # cleanup
+    app.dependency_overrides = {}
