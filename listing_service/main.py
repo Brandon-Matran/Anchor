a3004048b9421c506b763925ab536a54d3d26762
Allen <achenins@gmail.com>
2023-01-22 20:11:24 -0700

    completed update listings front end, added update listings to app

--- listing_service/main.py
+++ listing_service/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import listings
 
 
@@ -18,4 +19,3 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(listings.router)
-
